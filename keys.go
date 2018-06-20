602fe8120d35c830ccc76e872e4df544144dc77d
Ethan Lewis <elewis@kochava.com>
2018-06-20 11:02:22 -0700

    feature(keys) : added public and private keys along with simple test

---
+++ keys.go
@@ -0,0 +1,79 @@
+package crypto
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"math/big"
+)
+
+// Signature is a type representing an ECDSA signature
+type Signature struct {
+	R *big.Int
+	S *big.Int
+}
+
+// TODO (elewis) : serialize signature to bytes
+// func (sig *Signature) Serialize() []byte
+
+// PrivateKey wraps an ecdsa.PrivateKey to provide convenience functions
+type PrivateKey ecdsa.PrivateKey
+
+// PubKey returns the public key assoicated with this private key
+func (prvk *PrivateKey) PubKey() *PublicKey {
+	return (*PublicKey)(&prvk.PublicKey)
+}
+
+// ToECDSA returns the underlying ecsda Private key
+func (prvk *PrivateKey) ToECDSA() *ecdsa.PrivateKey {
+	return (*ecdsa.PrivateKey)(prvk)
+}
+
+// Sign generates an ECDSA signature for the provided hash using the private key.
+// **non deterministic**
+func (prvk *PrivateKey) Sign(hash []byte) (*Signature, error) {
+	r, s, err := ecdsa.Sign(rand.Reader, prvk.ToECDSA(), hash)
+	if err != nil {
+		return nil, err
+	}
+	return &Signature{R: r, S: s}, nil
+}
+
+// TODO (elewis) : Serialize to bytes
+// func (prvk *PrivateKey ) Serialize() []byte
+
+// PublicKey wraps an ecdsa.Public key for convenience
+type PublicKey ecdsa.PublicKey
+
+// ToECDSA returns the underlying ecsda Public Key
+func (pubk *PublicKey) ToECDSA() *ecdsa.PublicKey {
+	return (*ecdsa.PublicKey)(pubk)
+}
+
+// Verify the signature of hash value. Returns true if the signature is
+// generated with the private key assoicated with this public key
+func (pubk *PublicKey) Verify(hash []byte, sig *Signature) bool {
+	verified := ecdsa.Verify(pubk.ToECDSA(), hash, sig.R, sig.S)
+	return verified
+}
+
+// TODO (elewis) : Serialize to bytes
+// func (pubk *PublicKey ) Serialize() []byte
+
+// GenerateKeyPair create a private/public key pair using an elliptic curve
+func GenerateKeyPair(curve elliptic.Curve) (*PrivateKey, *PublicKey, error) {
+	prvKey, err := ecdsa.GenerateKey(curve, rand.Reader)
+	if err != nil {
+		return nil, nil, err
+	}
+	return (*PrivateKey)(prvKey), (*PublicKey)(&prvKey.PublicKey), nil
+}
+
+// GenerateKeyPairP256 create a private/public key pair using the P256 elliptic curve
+func GenerateKeyPairP256() (*PrivateKey, *PublicKey, error) {
+	prvKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		return nil, nil, err
+	}
+	return (*PrivateKey)(prvKey), (*PublicKey)(&prvKey.PublicKey), nil
+}
--- sha3.go
+++ sha3.go
@@ -2,11 +2,11 @@ package crypto
 
 import "golang.org/x/crypto/sha3"
 
-// Sha256 is a SHA-3-256 (not sha-256) hasher. Its generic security strength is
+// Sha3_256 is a SHA-3-256 hasher. Its generic security strength is
 // 256 bits against preimage attacks, and 128 bits against collision attacks.
 // data is an arbitrary length bytes slice
 // returns 32 bytes ( 256 bits ) hash of data.
-func Sha256(data ...[]byte) []byte {
+func Sha3_256(data ...[]byte) []byte {
 	d := sha3.New256()
 	for _, b := range data {
 		d.Write(b)
