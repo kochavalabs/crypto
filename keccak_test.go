5bc60d7cb7f64ff59177b3c710cc7ab657d67e46
Ethan Lewis <ethanlewis787@gmail.com>
2021-11-12 10:36:35 -0800

    fix: remove prefix from test seed data

--- keccak_test.go
+++ keccak_test.go
@@ -10,10 +10,10 @@ var keccakTestCases = []struct {
 	input    [][]byte
 	expected string
 }{
-	{&Keccak256Hasher{}, nil, "0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470"},
-	{&Keccak256Hasher{}, [][]byte{{}}, "0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470"},
-	{&Keccak256Hasher{}, [][]byte{[]byte("asdf")}, "0x4c8f18581c0167eb90a761b4a304e009b924f03b619a0c0e8ea3adfce20aee64"},
-	{&Keccak256Hasher{}, [][]byte{[]byte("asdf"), []byte("qwer")}, "0x68432ece0d2ea60cf463a6a9fae1b6deef509cb1e3b422df729ea6cc418ee8b5"},
+	{&Keccak256Hasher{}, nil, "c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470"},
+	{&Keccak256Hasher{}, [][]byte{{}}, "c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470"},
+	{&Keccak256Hasher{}, [][]byte{[]byte("asdf")}, "4c8f18581c0167eb90a761b4a304e009b924f03b619a0c0e8ea3adfce20aee64"},
+	{&Keccak256Hasher{}, [][]byte{[]byte("asdf"), []byte("qwer")}, "68432ece0d2ea60cf463a6a9fae1b6deef509cb1e3b422df729ea6cc418ee8b5"},
 }
 
 func TestKeccakHasherHex(t *testing.T) {
--- shake_test.go
+++ shake_test.go
@@ -10,10 +10,10 @@ var shakeTestCases = []struct {
 	input    [][]byte
 	expected string
 }{
-	{&Shake256Hasher{}, nil, "0x46b9dd2b0ba88d13233b3feb743eeb243fcd52ea62b81b82b50c27646ed5762f"},
-	{&Shake256Hasher{}, [][]byte{{}}, "0x46b9dd2b0ba88d13233b3feb743eeb243fcd52ea62b81b82b50c27646ed5762f"},
-	{&Shake256Hasher{}, [][]byte{[]byte("asdf")}, "0xf00c15643396616a89a0cb79039f740575defe9dbe307cccdaf8ae210e1c9cc6"},
-	{&Shake256Hasher{}, [][]byte{[]byte("asdf"), []byte("qwer")}, "0xc37f508e9af6afc86f9a768c15553036b7df09ba6d6fb3a836c1aa9a3bfc2b27"},
+	{&Shake256Hasher{}, nil, "46b9dd2b0ba88d13233b3feb743eeb243fcd52ea62b81b82b50c27646ed5762f"},
+	{&Shake256Hasher{}, [][]byte{{}}, "46b9dd2b0ba88d13233b3feb743eeb243fcd52ea62b81b82b50c27646ed5762f"},
+	{&Shake256Hasher{}, [][]byte{[]byte("asdf")}, "f00c15643396616a89a0cb79039f740575defe9dbe307cccdaf8ae210e1c9cc6"},
+	{&Shake256Hasher{}, [][]byte{[]byte("asdf"), []byte("qwer")}, "c37f508e9af6afc86f9a768c15553036b7df09ba6d6fb3a836c1aa9a3bfc2b27"},
 }
 
 func TestShakeHasherHex(t *testing.T) {
