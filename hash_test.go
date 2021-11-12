ae862dd2adb83aa5bfa0bb8322b417dd544478e5
Ethan M Lewis <elewis@kochava.com>
2021-11-12 11:16:12 -0800

    refactor: remove prefix values from keys (#23)
    * refactor: remove prefix values from keys
    
    * fix: remove prefix from test seed data

--- hash_test.go
+++ hash_test.go
@@ -1,19 +1,20 @@
 package crypto
 
 import (
-	"golang.org/x/crypto/sha3"
 	"reflect"
 	"testing"
+
+	"golang.org/x/crypto/sha3"
 )
 
 var sha3_256Hashes = []struct {
 	input    [][]byte
 	expected string
 }{
-	{nil, "0xa7ffc6f8bf1ed76651c14756a061d662f580ff4de43b49fa82d80a4b80f8434a"},
-	{[][]byte{{}}, "0xa7ffc6f8bf1ed76651c14756a061d662f580ff4de43b49fa82d80a4b80f8434a"},
-	{[][]byte{[]byte("asdf")}, "0xdd2781f4c51bccdbe23e4d398b8a82261f585c278dbb4b84989fea70e76723a9"},
-	{[][]byte{[]byte("asdf"), []byte("qwer")}, "0x06b7857261bcda1d351383b80bc2fb08d5957b61495ac73d7bd788f8f77e7c18"},
+	{nil, "a7ffc6f8bf1ed76651c14756a061d662f580ff4de43b49fa82d80a4b80f8434a"},
+	{[][]byte{{}}, "a7ffc6f8bf1ed76651c14756a061d662f580ff4de43b49fa82d80a4b80f8434a"},
+	{[][]byte{[]byte("asdf")}, "dd2781f4c51bccdbe23e4d398b8a82261f585c278dbb4b84989fea70e76723a9"},
+	{[][]byte{[]byte("asdf"), []byte("qwer")}, "06b7857261bcda1d351383b80bc2fb08d5957b61495ac73d7bd788f8f77e7c18"},
 }
 
 func TestGenericHasherHex(t *testing.T) {
--- hex.go
+++ hex.go
@@ -4,39 +4,14 @@ import (
 	"encoding/hex"
 )
 
-// Leading characters of a hex formatted string.
-const (
-	HexPrefix = "0x"
-)
-
-// encodes b as a hex string with 0x prefix.
+// ToHex encodes b as a hex string
 func ToHex(b []byte) string {
-	enc := make([]byte, len(b)*2+2)
-	copy(enc, HexPrefix)
-	hex.Encode(enc[2:], b)
-	return string(enc)
+	// consider checks for string size in the future
+	return hex.EncodeToString(b)
 }
 
 // FromHex returns the bytes represented by the hexadecimal string s.
-// s may be prefixed with "0x".
 func FromHex(hexEncoded string) ([]byte, error) {
-	if hasHexPrefix(hexEncoded) {
-		hexEncoded = hexEncoded[2:] // remove prefix
-	}
-
-	// If there is an odd number of characters prefix with 0 to correct
-	if len(hexEncoded)%2 == 1 {
-		hexEncoded = "0" + hexEncoded
-	}
-
-	hashAddress, err := hex.DecodeString(hexEncoded)
-	if err != nil {
-		return nil, err
-	}
-
-	return hashAddress, nil
-}
-
-func hasHexPrefix(str string) bool {
-	return len(str) >= 2 && str[0] == '0' && (str[1] == 'x' || str[1] == 'X')
+	// consider checks for string size in the future
+	return hex.DecodeString(hexEncoded)
 }
