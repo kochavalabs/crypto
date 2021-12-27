16131c765a177a4c54fae3e896570c0142bd9a82
Ethan Lewis <ethanlewis787@gmail.com>
2021-12-27 09:38:18 -0800

    fix: updated tests to use new private key length

--- ed25519_test.go
+++ ed25519_test.go
@@ -1,13 +1,12 @@
 package crypto
 
 import (
-	"crypto/ed25519"
 	"reflect"
 	"testing"
 )
 
 const Ed25519PubHex = "69ec35fafe61e514f4d2e54279671ab7e1e7fee9c4356da912ecd9f49db06773"
-const Ed25519PrivHex = "3a547668e859fb7b112a1e2dd7efcb739176ab8cfd1d9f224847fce362ebd99c"
+const Ed25519PrivHex = "3a547668e859fb7b112a1e2dd7efcb739176ab8cfd1d9f224847fce362ebd99c69ec35fafe61e514f4d2e54279671ab7e1e7fee9c4356da912ecd9f49db06773"
 const EdMessageHex = "91b5963ab438f4ddf9dbdda98d50eab56fb8dbab24242fd86997ed99d89f0869549ea309d697e6d072c479ff1464b4831c902e40b45e181df506b6cda5be36f95cd9023270e902e2ad7ce1c1e09545fc25733cd9d155f6c65bac93006ac9f9c6a2fcf912e5fbf49277d553576b9853900c906adde560"
 const EdSignatureHex = "b5aa14ad195fa99bcc1a0e77eb92903d6eb5fb387c3b71a3d19ac80ec86c1615251cc2ee0e06ba91319e5f56b893b03fb1160aa8dab4a72f69aa77be92d98d0c"
 
@@ -26,7 +25,7 @@ func TestEd25519Sign(t *testing.T) {
 	toSign, _ := FromHex(EdMessageHex)
 	privKey, _ := FromHex(Ed25519PrivHex)
 	signer := ed25519Signer{
-		privKey: ed25519.NewKeyFromSeed(privKey),
+		privKey: privKey,
 	}
 	signature, sigErr := signer.Sign(toSign)
 
